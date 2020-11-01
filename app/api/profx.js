eb44fd7a6fe05a659b7ef4b4e20e1664faa6518f
learningprocesss <minerva.mattia.4j@gmail.com>
2020-11-01 21:04:55 +0100

    service layer

--- app/api/profx.js
+++ app/api/profx.js
@@ -1,5 +1,6 @@
+
 function fx() {
-    this.url = "http://localhost:3000/proxy/http://WS2019AG/Procad/Profile/AppServer/rest/FINCAD/secure/Procad.ProfileNT.Services.MobilityService.IMobilityService"
+    this.url = "http://localhost:3001/http://WS2019AG/Procad/Profile/AppServer/rest/FINCAD/secure/Procad.ProfileNT.Services.MobilityService.IMobilityService"
     this.user = "procad"
     this.password = "procaf17"
 }
@@ -14,7 +15,8 @@ fxServiceBase.prototype.fxFetch = async function (params) {
 }
 
 function fxServiceLoadEntity() {
-
+    this.fx = new fx()
+    this.url = `${this.fx.url}/LoadEntity`
 }
 
 function fxServiceScalarQuery() {
@@ -24,10 +26,10 @@ function fxServiceScalarQuery() {
 
 function fxServiceCollectionQuery() {
     this.fx = new fx()
-    this.url = `${this.fx.url}/ExecuteCollectionQuery`
+    this.url = `${this.fx.url}/ExecuteCollectionQuery` ///ExecuteCollectionQuery
 }
 
-fxServiceCollectionQuery.prototype.fxFetch = async function () {
+fxServiceLoadEntity.prototype.fxFetch = async function () {
 
     let response
 
@@ -39,7 +41,11 @@ fxServiceCollectionQuery.prototype.fxFetch = async function () {
                 'Content-Type': 'application/json',
                 'Authorization': 'Basic ' + btoa(`${this.fx.user}:${this.fx.password}`)
             },
-            body: JSON.stringify('[{"collectionQuery": "(context) => context.Documents().Where(item => item.DocumentType == \"/Document/Vpn/\").Select(item => item.ToDictionary(FieldValueFilter.ExcludeDefault))"}]')
+            body: JSON.stringify([
+                {
+                    key: "urn:key:Document:19541"
+                }
+            ])
         })
 
         return await response.json()
@@ -47,28 +53,185 @@ fxServiceCollectionQuery.prototype.fxFetch = async function () {
         console.log(error);
     }
 
-    
-    return null    
+    return null
 }
 
-// class baseFxService {
-//     constructor() {
-//         this.fx = new fx()
-//     }
+fxServiceCollectionQuery.prototype.fxFetch = async function () {
 
+    let response
 
-// }
-
-async function getDocumentsByType(params) {
-
-    const collectionService = new fxServiceCollectionQuery()
+    var raw = "[{\"collectionQuery\": \"(context) => context.Documents().Where(item => item.DocumentType == \\\"/Document/Vpn/\\\").Select(item => item.ToDictionary(FieldValueFilter.ExcludeDefault))\"}]";
 
     try {
-        const data = await collectionService.fxFetch()
+        response = await fetch(this.url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(`${this.fx.user}:${this.fx.password}`)
+            },
+            body: raw
+        })
 
+        return await response.json()
     } catch (error) {
         console.log(error);
     }
 
-    console.log(data);
+
+    return null
+}
+
+class FxCore {
+    /**
+     * 
+     * @param {string} server 
+     * @param {string} tenant 
+     * @param {string} user 
+     * @param {string} password 
+     */
+    constructor(server, tenant, user, password) {
+        this.url = `http://localhost:3001/http://${server}/Procad/Profile/AppServer/rest/${tenant}/secure/Procad.ProfileNT.Services.MobilityService.IMobilityService`
+        this.user = user
+        this.password = password
+    }
+}
+
+const entities = {
+    DOCUMENT: 'Document',
+    PART: 'Part',
+    PROJECT: 'Project'
+}
+
+class Query {
+    /** @type {(string|Array|Object)} */
+    _data
+
+    /** @type {(string)} */
+    _queryMethod
+    /**
+     * 
+     * @param {string} entity 
+     */
+    constructor(entity) {
+        this.entity = entity
+    }
+
+    getData() {
+        return this._data
+    }
+
+    getQueryMethod() {
+        return this._queryMethod
+    }
+}
+
+class LoadEntityQuery extends Query {
+
+    /**
+    * 
+    * @param {string} entity 
+    * @param {number} id 
+    */
+    constructor(entity, id) {
+        super(entity)
+        this._queryMethod = "LoadEntity"
+        this._data = JSON.stringify([{ key: `urn:key:${this.entity}:${id}` }])
+    }
+}
+
+class ExecuteCollectionQuery extends Query {
+
+    /**
+    * 
+    * @param {string} entity 
+    */
+    constructor(entity) {
+        super(entity)
+        this._queryMethod = "ExecuteCollectionQuery"
+        this._data = "[{\"collectionQuery\": \"(context) => context.Documents().Where(item => item.DocumentType == \\\"/Document/Vpn/\\\").Select(item => item.ToDictionary(FieldValueFilter.ExcludeDefault))\"}]"
+    }
+}
+
+class FxServiceBase {
+
+    /**
+     * @type {RequestInit}
+     */
+    request
+
+    /**
+     * 
+     * @param {FxCore} fxCore 
+     * @param {Query} query 
+     */
+    constructor(fxCore, query) {
+
+        this.fxCore = fxCore
+        this.query = query
+        this.url = `${this.fxCore.url}/${query.getQueryMethod()}`
+        this.request = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(`${this.fxCore.user}:${this.fxCore.password}`)
+            }
+        }
+
+        // if (this.constructor === FxServiceBase) {
+        //     throw new Error("Can't instantiate abstract class!");
+        // }
+    }
+
+    /** 
+     * @param {string} endpoint 
+     */
+    setUrl(endpoint) {
+        this.url = `${this.fxCore.url}/${endpoint}`
+    }
+
+    async sendRequest(data) {
+        // throw new Error("Abstract method!");
+
+        this.request.body = this.query.getData()
+
+        const response = await fetch(this.url, this.request)
+
+        return await response.json()
+    }
+}
+
+class FxServiceLoadEntity extends FxServiceBase {
+    constructor(fxCore) {
+        super(fxCore)
+        this.setUrl("LoadEntity")
+    }
+
+    async sendRequest(data) {
+        this.request.body = JSON.stringify(data)
+        await fetch(this.url, this.request)
+    }
+}
+
+async function getDocumentsByType(params) {
+
+    const fxCore = new FxCore("WS2019AG", "FINCAD", "procad", "procaf17")
+
+    // const service = new fxServiceCollectionQuery()
+
+    const service = new FxServiceBase(fxCore, new ExecuteCollectionQuery("Document"))
+
+    try {
+        // const data = await service.fxFetch()
+
+        const data = await service.sendRequest()
+
+        console.log(data);
+
+        return data[1]
+
+    } catch (error) {
+        console.log(error);
+    }
 }
--- app/app.js
+++ app/app.js
@@ -1,10 +1,8 @@
 window.onload = async () => {
 
-    const dataContainer = document.getElementById("dataContainer")
 
-    console.log(dataContainer);
 
-   await loadData()
+    await loadData()
 
     setTimeout(() => {
 
@@ -13,6 +11,27 @@ window.onload = async () => {
 
 async function loadData() {
 
-    await getDocumentsByType()
+    const data = await getDocumentsByType()
 
+    const dataContainer = document.getElementById("dataContainer")
+
+    data.forEach(proData => {
+        const card = document.createElement("div")
+
+        card.setAttribute("class", "card")
+
+        const cardHeader = document.createElement("header")
+
+        cardHeader.setAttribute("class", "card-header")
+
+        const text = document.createTextNode(proData["/Document/docDescriptionUpdate"])
+
+        cardHeader.appendChild(text)
+
+        card.appendChild(cardHeader)
+    })
+
+
+
+    console.log(dataContainer);
 }
